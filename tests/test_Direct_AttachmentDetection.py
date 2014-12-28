accfc0f45abbc568afd982890910964e497f301a
Andrew Ferrier <andrewferrier@example.com>
2014-12-28 11:51:37 +0000

    Refactor FaultyAttachments to work as AttachmentDetection.

--- tests/test_Direct_AttachmentDetection.py
+++ tests/test_Direct_AttachmentDetection.py
@@ -2,100 +2,92 @@ from email.mime.multipart import MIMEMultipart
 
 import os
 
-from tests import BaseTestClasses
+from tests.BaseTestClasses import Email2PDFTestCase
 
 
-class AttachmentDetection(BaseTestClasses.Email2PDFTestCase):
+class AttachmentDetection(Email2PDFTestCase):
     def setUp(self):
         super(AttachmentDetection, self).setUp()
         self.msg = MIMEMultipart()
 
-    def test_pdfAsOctetStream(self):
+    def test_pdf_as_octet_stream(self):
         self.addHeaders()
         self.attachText("Some basic textual content")
         filename = self.attachPDF("Some PDF content", mainContentType="application", subContentType="octet-stream")
-        (rc, output, error) = self.invokeAsSubprocess()
-        self.assertEqual(0, rc)
+        error = self.invokeDirectly()
         self.assertEqual('', error)
         self.assertTrue(self.existsByTime())
         self.assertTrue(os.path.exists(os.path.join(self.workingDir, filename)))
         self.assertRegex(self.getPDFText(self.getTimedFilename()), "Some basic textual content")
         self.assertRegex(self.getPDFText(os.path.join(self.workingDir, filename)), "Some PDF content")
 
-    def test_pdfWithInvalidExtension(self):
+    def test_pdf_with_invalid_extension(self):
         self.addHeaders()
         self.attachText("Some basic textual content")
         filename = self.attachPDF("Some PDF content", extension="pdf")
-        (rc, output, error) = self.invokeAsSubprocess()
-        self.assertEqual(0, rc)
+        error = self.invokeDirectly()
         self.assertEqual('', error)
         self.assertTrue(self.existsByTime())
         self.assertTrue(os.path.exists(os.path.join(self.workingDir, filename)))
         self.assertRegex(self.getPDFText(self.getTimedFilename()), "Some basic textual content")
         self.assertRegex(self.getPDFText(os.path.join(self.workingDir, filename)), "Some PDF content")
 
-    def test_pdfAsOctetStreamWithInvalidExtension(self):
+    def test_pdf_as_octet_stream_with_invalid_extension(self):
         self.addHeaders()
         self.attachText("Some basic textual content")
         filename = self.attachPDF("Some PDF content", extension="xyz", mainContentType="application", subContentType="octet-stream")
-        (rc, output, error) = self.invokeAsSubprocess()
-        self.assertEqual(0, rc)
+        error = self.invokeDirectly()
         self.assertEqual('', error)
         self.assertTrue(self.existsByTime())
         self.assertTrue(os.path.exists(os.path.join(self.workingDir, filename)))
         self.assertRegex(self.getPDFText(self.getTimedFilename()), "Some basic textual content")
         self.assertRegex(self.getPDFText(os.path.join(self.workingDir, filename)), "Some PDF content")
 
-    def test_pdfAsOctetStreamNoBody(self):
+    def test_pdf_as_octet_stream_no_body(self):
         self.addHeaders()
         self.attachText("Some basic textual content")
         filename = self.attachPDF("Some PDF content", mainContentType="application", subContentType="octet-stream")
-        (rc, output, error) = self.invokeAsSubprocess(extraParams=['--no-body'])
-        self.assertEqual(0, rc)
+        error = self.invokeDirectly(extraParams=['--no-body'])
         self.assertEqual('', error)
         self.assertFalse(self.existsByTime())
         self.assertTrue(os.path.exists(os.path.join(self.workingDir, filename)))
         self.assertRegex(self.getPDFText(os.path.join(self.workingDir, filename)), "Some PDF content")
 
-    def test_jpegAsOctetStream(self):
+    def test_jpeg_as_octet_stream(self):
         self.addHeaders()
         self.attachText("Some basic textual content")
         imageFilename = self.attachImage(jpeg=True, content_type="application/octet-stream")
-        (rc, output, error) = self.invokeAsSubprocess()
-        self.assertEqual(0, rc)
+        error = self.invokeDirectly()
         self.assertEqual('', error)
         self.assertTrue(self.existsByTime())
         self.assertTrue(os.path.exists(os.path.join(self.workingDir, imageFilename)))
         self.assertRegex(self.getPDFText(self.getTimedFilename()), "Some basic textual content")
 
-    def test_jpegWithInvalidExtension(self):
+    def test_jpeg_with_invalid_extension(self):
         self.addHeaders()
         self.attachText("Some basic textual content")
         imageFilename = self.attachImage(jpeg=True, extension="blah")
-        (rc, output, error) = self.invokeAsSubprocess()
-        self.assertEqual(0, rc)
+        error = self.invokeDirectly()
         self.assertEqual('', error)
         self.assertTrue(self.existsByTime())
         self.assertTrue(os.path.exists(os.path.join(self.workingDir, imageFilename)))
         self.assertRegex(self.getPDFText(self.getTimedFilename()), "Some basic textual content")
 
-    def test_jpegAsOctetStreamWithInvalidExtension(self):
+    def test_jpeg_as_octet_stream_with_invalid_extension(self):
         self.addHeaders()
         self.attachText("Some basic textual content")
         imageFilename = self.attachImage(jpeg=True, content_type="application/octet-stream", extension="xyz")
-        (rc, output, error) = self.invokeAsSubprocess()
-        self.assertEqual(0, rc)
+        error = self.invokeDirectly()
         self.assertEqual('', error)
         self.assertTrue(self.existsByTime())
         self.assertTrue(os.path.exists(os.path.join(self.workingDir, imageFilename)))
         self.assertRegex(self.getPDFText(self.getTimedFilename()), "Some basic textual content")
 
-    def test_unidentifiedFile(self):
+    def test_unidentified_file(self):
         self.addHeaders()
         self.attachText("Some basic textual content")
         self.attachAttachment("application", "data", "some data in some format", "somefile.xyz")
-        (rc, output, error) = self.invokeAsSubprocess()
-        self.assertEqual(0, rc)
+        error = self.invokeDirectly()
         self.assertEqual('', error)
         self.assertTrue(self.existsByTime())
         self.assertFalse(os.path.exists(os.path.join(self.workingDir, "somefile.xyz")))
