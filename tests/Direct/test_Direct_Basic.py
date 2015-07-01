23f7f60432ae5cbffa9a7789d6bb99c0da6da2f2
Andrew Ferrier <andrewferrier@example.com>
2015-06-30 19:03:29 -0500

    Introduce support for headers with multiple encodings - closes #74

--- tests/Direct/test_Direct_Basic.py
+++ tests/Direct/test_Direct_Basic.py
@@ -1,3 +1,4 @@
+from email.header import Header
 from email.mime.multipart import MIMEMultipart
 
 import os
@@ -26,6 +27,24 @@ class Direct_Basic(BaseTestClasses.Email2PDFTestCase):
     def test_internationalised_subject2(self):
         path = os.path.join(self.examineDir, "internationalised_subject_jp.pdf")
         self.addHeaders(subject='=?iso-2022-jp?B?GyRCOiNHLyRiSSwkOiRkJGo/ayQyJGsbKEIhIRskQkcvS3ZBMCRO?=')
+        error = self.invokeDirectly(outputFile=path, extraParams=['--headers'])
+        self.assertTrue(os.path.exists(path))
+        self.assertEqual('', error)
+
+    def test_internationalised_subject3(self):
+        path = os.path.join(self.examineDir, "internationalised_subject_de.pdf")
+        self.addHeaders(subject='Ihre Anfrage, Giesestra=?utf-8?B?w58=?=e 5')
+        error = self.invokeDirectly(outputFile=path, extraParams=['--headers'])
+        self.assertTrue(os.path.exists(path))
+        self.assertEqual('', error)
+
+    def test_internationalised_subject4(self):
+        path = os.path.join(self.examineDir, "internationalised_subject_complex.pdf")
+        header = Header()
+        header.append(bytes('£100', 'iso-8859-1'), 'iso-8859-1')
+        header.append(bytes(' is != how much ', 'utf-8'), 'utf-8')
+        header.append(bytes('I have to spend!', 'iso-8859-15'), 'iso-8859-15')
+        self.addHeaders(subject=header.encode())
         error = self.invokeDirectly(outputFile=path, extraParams=['--headers'])
         self.assertTrue(os.path.exists(path))
         self.assertEqual('', error)
